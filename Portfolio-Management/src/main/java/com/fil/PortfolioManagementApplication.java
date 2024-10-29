0ce23a46fdb43c7cdd90dd5f85766d5782ad5458
Yashpal Rout <yashrout10@gmail.com>
2024-10-29 16:56:19 +0530

    (Feature): Initialize backend

---
+++ Portfolio-Management/src/main/java/com/fil/PortfolioManagementApplication.java
@@ -0,0 +1,13 @@
+package com.fil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioManagementApplication.class, args);
+	}
+
+}
---
+++ Portfolio-Management/src/test/java/com/fil/PortfolioManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
